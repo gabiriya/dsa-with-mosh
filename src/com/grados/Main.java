b165defdbe90cb47fa8c49b8fea036631865ace7
gabiriya <youssef.lotfi9@gmail.com>
2022-03-03 21:45:02 +0100

    implementing stack

--- src/com/grados/Main.java
+++ src/com/grados/Main.java
@@ -1,11 +1,20 @@
 package com.grados;
 
 import com.grados.mosh.stack.BalancedExpression;
+import com.grados.mosh.stack.MyStack;
+
+import java.util.Stack;
 
 public class Main {
 
     public static void main(String[] args) {
-        BalancedExpression b = new BalancedExpression();
-        System.out.println(b.isItBalanced("({}{}{})"));
+        MyStack stack = new MyStack();
+        System.out.println(stack.isEmpty());
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+        stack.push(40);
+        stack.pop();
+        stack.printIt();
     }
 }
--- src/com/grados/mosh/stack/MyStack.java
+++ src/com/grados/mosh/stack/MyStack.java
@@ -1,6 +1,50 @@
 package com.grados.mosh.stack;
 
 public class MyStack {
-    private int test;
+
+    private int size = 1;
+    public int[] array;
+
+    public MyStack() {
+        array = new int[size];
+    }
+
+    // push
+    public void push(int data){
+        array[size++ - 1] = data;
+        int[] arr = new int[size];
+        System.arraycopy(array, 0, arr, 0, size-1);
+        array = arr;
+    }
+
+    // pop
+    public int pop(){
+        int[] arr = new int[size-1];
+        var last = array[size-2];
+        System.arraycopy(array, 0, arr, 0, size-2);
+        array = arr;
+        return last;
+    }
+
+    // peek
+    public int peek(){
+        if (!isEmpty())
+            return array[size-2];
+        else throw new IllegalArgumentException();
+    }
+
+
+    // isEmpty
+    public boolean isEmpty(){
+        return size == 1;
+    }
+    // printIt
+    public void printIt(){
+        System.out.print("[");
+        for (int i= 0; i< size-2 ; i++){
+            System.out.print( array[i] + ",");
+        }
+        System.out.print("]");
+    }
 }
 
