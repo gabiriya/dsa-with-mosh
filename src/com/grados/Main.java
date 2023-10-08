361eb47feaf45b964296d4af6fe5f5e8a3199ce6
= <=>
2023-10-08 11:03:33 +0100

    refactor packages

--- src/com/grados/Main.java
+++ src/com/grados/Main.java
@@ -1,22 +1,7 @@
 package com.grados;
 
-
-import com.grados.mywork.part1.trees.BinaryTree;
-
 public class Main {
 
     public static void main(String[] args) {
-        BinaryTree tree = new BinaryTree();
-        tree.insert(10);
-        tree.insert(15);
-        tree.insert(5);
-        tree.insert(7);
-        tree.insert(2);
-        tree.insert(6);
-        tree.insert(8);
-        tree.insert(1);
-        System.out.println("printing min val");
-        var x = tree.minValue();
-        System.out.println(x);
     }
 }
